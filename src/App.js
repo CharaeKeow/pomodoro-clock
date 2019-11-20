898f39ac8942019b1cb04c71f4dbdd6fdb3a76a3
CharaeKeow <charaekeow11@gmail.com>
2019-11-21 02:08:51 +0800

    control  components - up, down, name, length

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+class Control extends React.Component {
+  render() {
+    return(
+      <div>
+        <div id={this.props.ID}>{this.props.name}</div>        
+        <div className="control">
+          <div className="btn" id={this.props.increment}>&#x025B4;</div>
+          <div id={this.props.lengthID}>{this.props.value}</div>
+          <div className="btn" id={this.props.decrement}>&#x025BE;</div>                
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pomodoro Clock</h1>
+      <Control 
+      name="Break Length" 
+      ID="break-label" 
+      increment="break-increment" 
+      decrement="break-decrement" 
+      lengthID="break-length" 
+      value="5"/>
+      <Control 
+      name="Session Length" 
+      ID="session-label" 
+      increment="session-increment" 
+      decrement="session-decrement" 
+      lengthID="session-length" 
+      value="25"/>
+           
     </div>
   );
 }
