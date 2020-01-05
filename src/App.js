cd72e3a68414707cf0ebe26785ce71eb0d5603b0
Charae Eh Sin <charaekeow11@gmail.com>
2020-01-06 02:26:41 +0800

    change the state to parent component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,92 @@
 import React from 'react';
 import './App.css';
 
-class Break extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 5
+      break: 5,
+      session: 25,
     }
-    //this.updateTime = this.updateTime.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.increment = this.increment.bind(this);
+    this.decrementBreak = this.decrementBreak.bind(this);
+    this.incrementBreak = this.incrementBreak.bind(this);
+    this.decrementSession = this.decrementSession.bind(this);
+    this.incrementSession = this.incrementSession.bind(this);
   }
 
-  decrement() {
-    if (this.state.time > 0) {
+  decrementBreak() {
+    if (this.state.break > 0) {
       this.setState({
-        time: this.state.time - 1
+        break: this.state.break - 1
       })
     }
   }
 
-  increment() {
-    if (this.state.time < 60) {
+  incrementBreak() {
+    if (this.state.break < 60) {
       this.setState({
-        time: this.state.time + 1
+        break: this.state.break + 1
+      })
+    }
+  }
+
+  decrementSession() {
+    if (this.state.session > 0) {
+      this.setState({
+        session: this.state.session - 1
+      })
+    }
+  }
+
+  incrementSession() {
+    if (this.state.session < 60) {
+      this.setState({
+        session: this.state.session + 1
       })
     }
   }
 
   render() {
     return(
+      <div className="app">
+      <h1>Pomodoro Clock</h1>
+      <div className="control-pnl">
+        <Break 
+          time={this.state.break}
+          increment={this.incrementBreak}
+          decrement={this.decrementBreak}
+        />
+        <Session 
+          time={this.state.session}
+          increment={this.incrementSession}
+          decrement={this.decrementSession}
+        />
+      </div>
+      <div className="status">        
+      </div>                 
+    </div>
+    )
+  }
+}
+
+class Break extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: this.props.time
+    }
+  }
+
+  
+
+  render() {
+    return(
       <div>
         <div id="break-label">Break</div>
         <div className="control">
-          <div id="break-decrement" className="btn" onClick={this.decrement}> - </div>
-          <div id="break-length">{this.state.time}</div>
-          <div id="break-increment" className="btn" onClick={this.increment}> + </div>
+          <div id="break-decrement" className="btn" onClick={this.props.decrement}> - </div>
+          <div id="break-length">{this.props.time}</div>
+          <div id="break-increment" className="btn" onClick={this.props.increment}> + </div>
         </div>
       </div>
     )
@@ -46,26 +97,7 @@ class Session extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 25
-    }
-    //this.updateTime = this.updateTime.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.increment = this.increment.bind(this);
-  }
-
-  decrement() {
-    if (this.state.time > 0) {
-      this.setState({
-        time: this.state.time - 1
-      })
-    }
-  }
-
-  increment() {
-    if (this.state.time < 60) {
-      this.setState({
-        time: this.state.time + 1
-      })
+      time: this.props.time
     }
   }
 
@@ -74,28 +106,15 @@ class Session extends React.Component {
       <div>
         <div id="break-label">Break</div>
         <div className="control">
-          <div id="break-decrement" className="btn" onClick={this.decrement}> - </div>
-          <div id="break-length">{this.state.time}</div>
-          <div id="break-increment" className="btn" onClick={this.increment}> + </div>
+          <div id="break-decrement" className="btn" onClick={this.props.decrement}> - </div>
+          <div id="break-length">{this.props.time}</div>
+          <div id="break-increment" className="btn" onClick={this.props.increment}> + </div>
         </div>
       </div>
     )
   }
 }
 
-function App() {
-  return (
-    <div className="app">
-      <h1>Pomodoro Clock</h1>
-      <div className="control-pnl">
-        <Break/>
-        <Session />
-      </div>
-      <div className="status">
-        
-      </div>                 
-    </div>
-  );
-}
+
 
 export default App;
