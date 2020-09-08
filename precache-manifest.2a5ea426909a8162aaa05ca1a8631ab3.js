b75af1e9fa9e37ac2205e3839e597b28f16e8fb3
CharaeKeow <charaekeow11@gmail.com>
2020-09-08 12:37:01 +0800

    Updates

---
+++ precache-manifest.2a5ea426909a8162aaa05ca1a8631ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106185c97cd70d29013c65480f373608",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "ac24f3233af2cfcef74b",
+    "url": "/pomodoro-clock/static/css/main.16ee7e91.chunk.css"
+  },
+  {
+    "revision": "5d5499f9254eb6ff03c3",
+    "url": "/pomodoro-clock/static/js/2.876be8de.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/pomodoro-clock/static/js/2.876be8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac24f3233af2cfcef74b",
+    "url": "/pomodoro-clock/static/js/main.208e3680.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
